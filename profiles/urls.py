7547786c1fc56504daf2498c6f5e2ef02868da7b
Dina Kidane <dinakidane@hotmail.com>
2024-09-21 17:07:04 +0000

    add profiles views

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,5 @@ from profiles import views
 
 urlpatterns = [
     path('profiles/', views.UserProfileListView.as_view()),
+    path('profiles/<int:pk>/', views.UserProfileDetailView.as_view()),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import UserProfile
@@ -13,4 +15,27 @@ class UserProfileListView(APIView):
         user_profiles = UserProfile.objects.all()
         serialized_profiles = UserProfileSerializer(user_profiles, many=True)
         return Response(serialized_profiles.data)
+
+class UserProfileDetailView(APIView):
+    serializer_class = UserProfileSerializer
+
+    def get_object(self, pk):
+        try:
+            user_profile = UserProfile.objects.get(pk=pk)
+            return user_profile
+        except UserProfile.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        user_profile = self.get_object(pk)
+        serializer = UserProfileSerializer(user_profile)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        user_profile = self.get_object(pk)
+        serializer = UserProfileSerializer(user_profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 

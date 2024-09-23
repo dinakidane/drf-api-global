cb1a0de63d141766aa9055aa32d34023d3f72b23
Dina Kidane <dinakidane@hotmail.com>
2024-09-23 19:22:05 +0000

    add image size constraints

--- posts/serializers.py
+++ posts/serializers.py
@@ -8,6 +8,26 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')  # Read-only field for the owner's profile ID
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')  # Read-only field for the profile image URL
 
+
+    def check_image_constraints(self, image_file):
+        # Define new limits: 2.5MB for size, 4200px for dimensions
+        size_limit = 2.5 * 1024 * 1024  # 2.5MB
+        dimension_limit = 4200  # 4200px max height or width
+
+        # Ensure the file size is within the limit
+        if image_file.size > size_limit:
+            raise serializers.ValidationError('The image exceeds the size limit of 2.5MB.')
+
+        # Ensure the image dimensions are within the allowed range
+        if image_file.image.height > dimension_limit:
+            raise serializers.ValidationError('Image height cannot exceed 4200px.')
+        if image_file.image.width > dimension_limit:
+            raise serializers.ValidationError('Image width cannot exceed 4200px.')
+
+        return image_file
+
+
+
     def get_is_owner(self, obj):
         """
         Check if the requesting user is the owner of the post.
@@ -20,5 +40,5 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image'
+            'title', 'content', 'image', 'image_filter'
         ]

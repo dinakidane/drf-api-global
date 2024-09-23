fc0e1eb9524b9a594404320e9d7caf0f45fe5709
Dina Kidane <dinakidane@hotmail.com>
2024-09-23 19:03:12 +0000

    add image filter

--- posts/models.py
+++ posts/models.py
@@ -7,6 +7,24 @@ class Post(models.Model):
     Post model, related to 'owner', i.e. a User instance.
     Default image set so that we can always reference image.url.
     """
+    
+    image_filter_choices_list = [
+        ('clarendon', 'Clarendon'), 
+        ('gingham', 'Gingham'),
+        ('moon', 'Moon'), 
+        ('reyes', 'Reyes'),
+        ('perpetua', 'Perpetua'), 
+        ('amaro', 'Amaro'),
+        ('mayfair', 'Mayfair'), 
+        ('willow', 'Willow'),
+        ('juno', 'Juno'), 
+        ('slumber', 'Slumber'),
+        ('crema', 'Crema'), 
+        ('ludwig', 'Ludwig'),
+        ('aden', 'Aden'), 
+        ('brooklyn', 'Brooklyn')
+    ]
+
     owner = models.ForeignKey(User, on_delete=models.CASCADE)  # Link the post to a User
     created_at = models.DateTimeField(auto_now_add=True)  # Auto-set the time when the post is created
     updated_at = models.DateTimeField(auto_now=True)  # Auto-set the time whenever the post is updated

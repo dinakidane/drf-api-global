5937c0d27d7a308e0977fd25aac721ec6f8d6987
Dina Kidane <dinakidane@hotmail.com>
2024-09-25 16:51:33 +0000

    views for favourites

--- favourites/views.py
+++ favourites/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from .models import Favourite
+from .serializers import FavouriteSerializer
+from posts.models import Post
 
-# Create your views here.
+class FavouriteListCreateView(generics.ListCreateAPIView):
+    """
+    View to list and create favourites.
+    """
+    serializer_class = FavouriteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Favourite.objects.filter(user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        post_id = request.data.get('post')
+        post = get_object_or_404(Post, id=post_id)
+
+        if post.owner == request.user:
+            return Response({"error": "You cannot like your own post."}, status=status.HTTP_403_FORBIDDEN)
+
+        return super().create(request, *args, **kwargs)
+
+class FavouriteDetailView(generics.DestroyAPIView):
+    """
+    View to delete a favourite (unlike a post).
+    """
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        post_id = self.kwargs['post_id']
+        favourite = get_object_or_404(Favourite, user=self.request.user, post_id=post_id)
+        return favourite
+
+    def delete(self, request, *args, **kwargs):
+        favourite = self.get_object()
+        self.perform_destroy(favourite)
+        return Response(status=status.HTTP_204_NO_CONTENT)

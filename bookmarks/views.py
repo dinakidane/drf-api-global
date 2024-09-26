e2cba7fba477b1f259425df918ee946d9748e781
Dina Kidane <dinakidane@hotmail.com>
2024-09-26 01:05:53 +0000

    bookmarks views

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Bookmark
+from .serializers import BookmarkSerializer
 
-# Create your views here.
+class BookmarkListCreateView(generics.ListCreateAPIView):
+    """
+    List all bookmarks of the logged-in user or add a bookmark to a post.
+    """
+    serializer_class = BookmarkSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Bookmark.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class BookmarkDetailView(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve or delete a bookmark (removes it).
+    """
+    serializer_class = BookmarkSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Bookmark.objects.filter(user=self.request.user)
